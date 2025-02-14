f79de307eef05a6573fe486c83691e371d06bc32
ajay <nagpalajay01@gmail.com>
2025-02-14 16:28:59 +0530

    updating Chapter_XV

--- sections.py
+++ sections.py
@@ -1,11 +1,11 @@
 import os
 
 # Define the target folder
-folder_name = "Chapter_XIX"  # Change this to your desired folder name
+folder_name = "Chapter_XV"  # Change this to your desired folder name
 
 # Define the range of chapters
-start = 351
-end = 357
+start = 270
+end = 297
 
 # Create the folder if it doesn't exist
 os.makedirs(folder_name, exist_ok=True)
